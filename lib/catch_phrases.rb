58ae1eb990842a63c29ae09968b2816f7bb0d456
William Famor <jaredfamor@gmail.com>
2019-09-29 00:24:22 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,17 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  puts "'It's-a me, Mario!'"
 end
 
 def toadstool
   puts status
+end
+
+def link
+  status = 'Its Dangerous To Go Alone! Take This'
+  puts phrase
+end
+
+def any_phrase
+  status = "Orange you glad I didn't say banana."
+  puts phrase
 end
